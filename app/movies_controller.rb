e0fbaf67af43e15d2dd05fbcb7b9f00e2f7db39a
Kevin Huang <yellow.khaos@gmail.com>
2018-02-17 22:33:50 +0000

    automatically backed up by learn

--- app/movies_controller.rb
+++ app/movies_controller.rb
@@ -42,14 +42,14 @@ def can_get_size_of_the_database
 end
 
 def can_find_the_first_item_from_the_database_using_id
-  Movie.first.title
+  Movie.id(1).title
 end
 
 def can_find_by_multiple_attributes
   # title == "Title"
   # release_date == 2000
   # director == "Me"
-  __
+  
 end
 
 def can_find_using_where_clause_and_be_sorted
